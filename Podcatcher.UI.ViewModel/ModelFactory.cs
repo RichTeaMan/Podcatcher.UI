31762cc4b791d5194735627de523c18d34e60628
Thomas Holmes <thomasholmes@outlook.com>
2015-11-14 12:29:35 +0000

    Added model factory for de/serialisation.

---
+++ Podcatcher.UI.ViewModel/ModelFactory.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Podcatcher.UI.ViewModel
+{
+    public class ModelFactory
+    {
+        public string Serialise(object model)
+        {
+            var json = JsonConvert.SerializeObject(model);
+            return json;
+        }
+
+        public T Deserialise<T>(string json)
+        {
+            var result = JsonConvert.DeserializeObject<T>(json);
+            return result;
+        }
+
+    }
+}
