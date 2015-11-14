696d6d02bfd75536650da23cfab71aaa9662d7bb
Thomas Holmes <thomasholmes@outlook.com>
2015-11-14 17:55:34 +0000

    Added track list.

--- Podcatcher.UI/PodcastPage.xaml.cs
+++ Podcatcher.UI/PodcastPage.xaml.cs
@@ -8,21 +8,36 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using Podcatcher.UI.ViewModel;
+using System.Threading.Tasks;
 
 namespace Podcatcher.UI
 {
     public partial class PodcastPage : PhoneApplicationPage
     {
+        public ItunesSearchResult ItunesSearchResult { get; set; }
+
         public PodcastPage()
         {
             InitializeComponent();
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        private async Task LoadRss()
+        {
+            
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
             var itunesSearchResult = this.GetParam<ItunesSearchResult>();
             DataContext = itunesSearchResult;
+            ItunesSearchResult = itunesSearchResult;
+            await ItunesSearchResult.LoadPodcasts();
+        }
+
+        private void StackPanel_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+
         }
     }
 }
